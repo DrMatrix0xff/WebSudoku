d6793851db1cdc278619eedb977a278e0b67fdba
Whoo Lishin <lixin.hu@ebuinfo.com>
2016-08-29 10:11:51 +0800

    more jquery & fix test

--- appcore/sudokuer.py
+++ appcore/sudokuer.py
@@ -15,6 +15,10 @@ def solve(puzzle):
     return f
 
 if __name__ == '__main__':
+    import tornado.ioloop
+    from tornado import gen
+    ev_loop = tornado.ioloop.IOLoop.instance().current()
+
     p = [
         0,0,3, 0,0,0, 0,0,0,
         0,0,0, 0,0,0, 0,0,0,
@@ -26,5 +30,12 @@ if __name__ == '__main__':
         0,0,0, 0,0,0, 0,0,0,
         0,0,0, 0,0,0, 0,0,0,
     ]
-    print solve(p)
+
+    @gen.coroutine
+    def test_solve():
+        result = yield solve(p)
+        print result
+
+    ev_loop.run_sync(test_solve)
+    ev_loop.close(all_fds=True)
 
--- static/js/solver.js
+++ static/js/solver.js
@@ -7,8 +7,11 @@ $(document).ready(
         var c, k;
         $(this).attr('disabled', 'disabled');
         for (k = 0; k < 81; k++) {
-          c = inputs[String(k)];
-          c.value = "";
+          c = $(inputs[String(k)]);
+          c.val("");
+          if (c.css("background-color")) {
+            c.css("background-color", "");
+          }
         }
         c0.focus(); 
         $(this).removeAttr('disabled');
@@ -18,13 +21,16 @@ $(document).ready(
       function () {
         $(this).attr('disabled', 'disabled');
         var pz = [];
-        var c, k, v;
+        var userin = [];
+        var c, k, v, vv;
         for (k = 0; k < 81; k++) {
-          c = inputs[String(k)];
-          if (c.value === '') {
+          c = $(inputs[String(k)]);
+          vv = c.val();
+          if (vv === '' || vv === '0') {
             v = 0;
           } else {
-            v = Number(c.value);
+            v = Number(vv);
+            userin.push(k);
           }
           pz.push(v);
         }
@@ -36,11 +42,17 @@ $(document).ready(
           },
           dataType: 'json',
           success: function (data, status, xhr) {
+            var idx;
             if (data.eno === 0) {
               for (var i = 0; i < data.solution.length; i++) {
                 var v = data.solution[i];
-                var c = inputs[String(i)];
-                c.value = String(v);
+                var c = $(inputs[String(i)]);
+                c.val(String(v));
+              }
+              for (i = 0; i < userin.length; i++) {
+                  idx = userin[i];
+                  c = $(inputs[String(idx)]);
+                  c.css("background-color", "#ccc");
               }
             } else {
               window.alert("Cannot Find Any Solution, Please Check Your Puzzle!");
